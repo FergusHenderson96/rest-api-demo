ecfd9561d99e35bfe9ccae14f206c3c94e5df3ea
FergusHenderson96 <FergusHenderson96@users.noreply.github.com>
2021-02-02 11:58:50 +0000

    all files updated to show correct workings for functioning backend

--- src/server.js
+++ src/server.js
@@ -1,58 +1,19 @@
 require("./db/connection");
-
-
 const express = require("express");
-const {User} = require("./model/User");
-const port = process.env.PORT || 5000
+const { userRouter } = require("./routes/users");
+const { postRouter } = require("./routes/posts");
+
+const port = process.env.PORT || 5000;
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
+app.use(userRouter);
+app.use(postRouter);
 
 app.get("/health", (req, res) => {
-    res.status(200).send({message: "API is working"})
-    });
-
-app.get("/users", async (req, res) => {
-    try {
-        const allUsers = await User.find({});
-        res.status(200).send(allUsers);
-    }catch (error) {
-        res.status(500).send(error);
-    }
+  res.status(200).send({ message: "API is working" });
 });
 
-//http://localhost:5000/users
-
-app.post("/users", async (req, res) => {
-    try {
-        const user = new User(req.body);
-        const savedUser = await user.save();
-        console.log(req.body);
-        res.status(201).send({message: "Successfully added to database"})
-    } catch (error) {
-        res.status(500).send({message: "Could not connect"})
-    }
-})
-
-app.patch("/user/:id", async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id, req.body, {new: true});
-        console.log(user)
-        res.status(200).send(user);
-    }catch (error) {
-        res.status(404).send({message: "user not found"});
-    }
-})
-
-app.delete("/user/:id", async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        res.status(200).send(user);
-    }catch (error) {
-        res.status(404).send({message: "user not found"});
-    }
-})
-
-
 app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
+  console.log(`Server is listening on port ${port}`);
 });
